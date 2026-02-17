a017c623703ccacb5516bdbb76945265dd5a5a4e
shams al labib <samslabib19@gmail.com>
2026-02-17 11:55:48 +0600

    register success

--- src/Firebase/Login/Register.jsx
+++ src/Firebase/Login/Register.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router";
+import { AuthContext } from "../Authentication/AuthContext";
+import Swal from "sweetalert2";
 
 const Register = () => {
+  const { createUser, googleLogin } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  
+
+  const handleGoogleLogin = () => {
+    googleLogin()
+      .then(() => {
+        Swal.fire({
+          title: "Login Success!",
+          text: "Conggrass you are now a ict-seba member!",
+          icon: "success",
+        });
+      })
+      .catch(() => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+          footer: '<a href="#">Why do I have this issue?</a>',
+        });
+      });
+  };
 
   const handleRegister = (data) => {
-    
+    createUser(data.email, data.password)
+      .then(() => {
+        Swal.fire({
+          title: "Login Success!",
+          text: "Conggrass you are now a ict-seba member!",
+          icon: "success",
+        });
+      })
+      .catch(() => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+          footer: '<a href="#">Why do I have this issue?</a>',
+        });
+      });
   };
 
   return (
@@ -75,6 +111,43 @@ const Register = () => {
                 <a className="link link-hover">Forgot password?</a>
               </div>
               <button className="btn btn-neutral mt-4">Register</button>
+
+              <p className="my-2 text-center">OR</p>
+              {/* Google */}
+              <button
+                onClick={() => handleGoogleLogin()}
+                type="button"
+                className="btn bg-white text-black border-[#e5e5e5]"
+              >
+                <svg
+                  aria-label="Google logo"
+                  width="16"
+                  height="16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <g>
+                    <path d="m0 0H512V512H0" fill="#fff"></path>
+                    <path
+                      fill="#34a853"
+                      d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                    ></path>
+                    <path
+                      fill="#4285f4"
+                      d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                    ></path>
+                    <path
+                      fill="#fbbc02"
+                      d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                    ></path>
+                    <path
+                      fill="#ea4335"
+                      d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                    ></path>
+                  </g>
+                </svg>
+                Login with Google
+              </button>
             </fieldset>
             <p>
               Already have an account?{" "}
