9178cf252e28c241540432a09879d654bbb8fa08
shams al labib <samslabib19@gmail.com>
2026-02-17 10:54:07 +0600

    authentication added

--- src/Firebase/Authentication/AuthProvider.jsx
+++ src/Firebase/Authentication/AuthProvider.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../Config file/_firebase.init";
 
 const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = async (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const googleLogin = async () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
   const info = {
-    name: "shams",
+    googleLogin,
+    createUser,
   };
 
   return <AuthContext value={info}>{children}</AuthContext>;
